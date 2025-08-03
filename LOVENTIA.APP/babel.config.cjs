5e377c4fe5f0c611f8c87cac1a6fabd044af3cf8
Ville1981 <villehermaala1981@gmail.com>
2025-08-03 16:28:41 +0200

    Remove nested LOVENTIA.APP folder and flatten structure

--- LOVENTIA.APP/babel.config.cjs
+++
@@ -1,8 +0,0 @@
-// babel.config.cjs
-
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current', browsers: 'last 2 versions' } }],
-    '@babel/preset-react'
-  ]
-};
--- LOVENTIA.APP/src/components/discover/index.js
+++
@@ -1,3 +0,0 @@
-// client/src/components/discover/index.js
-export { default as ProfileCard } from "../UserCard";
-export { default as ProfileCardList } from "../UserCardList";
