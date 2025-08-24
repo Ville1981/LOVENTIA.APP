5c2d089e5ef4680def9953ec7b96f993e7775c51
Ville1981 <villehermaala1981@gmail.com>
2025-08-24 20:45:08 +0200

    cleanup: remove duplicate client/server and verify working state

--- _pack/light-20250824-191531/server/controllers/utils/getUserId.js
+++
@@ -1,9 +0,0 @@
-// --- REPLACE START: normalized user id extractor ---
-/**
- * Normalized way to read the authenticated user's id from request.
- * Works with the upgraded authenticate middleware ({ userId, role, ... }).
- */
-export default function getUserId(req) {
-  return req?.user?.userId || req?.userId || null;
-}
-// --- REPLACE END ---
--- _pack/light-20250824-191531/server/src/routes/message.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Shim: forwards to actual message route
-module.exports = require('../../routes/message.js');
